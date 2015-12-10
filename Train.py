6bd5e24fbbb1247ee4cd9cda67faa5c6bcfa2991
Akhil Raj Azhikodan <akhil95raj@gmail.com>
2015-12-11 02:14:50 +0530

    Training for Sentiment analysis from static training corpus
    positive and negative txts are the training corpus files.

---
+++ Train.py
@@ -0,0 +1,46 @@
+import re
+
+vocabulary =[]
+positve = []
+negative = []
+
+def readfile():
+    """fucntion to get the data from the training corpus"""
+    print("\n  -reading positive training corpus...")
+    f = open("positive.txt",'r')
+    for line in f:
+        line = re.sub('[!@#$.:{}()""-=;,]', '', line)
+        for word in line.split():
+           positve.append(word)
+           if(word not in vocabulary):
+               vocabulary.append(word)
+    print("\n  -reading positive training corpus...")
+    f = open("negative.txt",'r')
+    for line in f:
+        line = re.sub('[!@#$.:{}()""-=;,]', '', line)
+        for word in line.split():
+            negative.append(word)
+            if(word not in vocabulary):
+               vocabulary.append(word)
+
+
+def buildProbabilityTable():
+    "creates the probabilty table for each word in the vocabulary"
+    f = open('Table','w')
+    vocabularysize = len(vocabulary);
+
+    for word in vocabulary:
+         positivecount = positve.count(word)
+         negativecount = negative.count(word)
+         totalcount = positivecount+negativecount
+         probpositive = (positivecount+1)/float(totalcount+vocabularysize)
+         probnegative = (negativecount+1)/float(totalcount+vocabularysize)
+         f.write(word+" "+str(probpositive)+" "+str(probnegative)+"\n")
+    f.close()
+
+
+def initialise():
+    print("\ncompiling the vocabulary from training corpus -")
+    readfile()
+    buildProbabilityTable()
+
